051134f501fe711cffab6e6676774633d1c3580d
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2021-10-18 13:28:36 +0800

    init

--- typescript/src/App.vue
+++ typescript/src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <test msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    test
+  }
+})
+</script>
 
 <style lang="scss">
 #app {
@@ -13,18 +28,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
---
+++ typescript/src/components/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    {{ a }}
+    {{ test(4, 'ss') }}
+  </div>
+</template>
+
+<script setup lang="ts">
+let a = 0
+a = 'sss'
+console.log(a)
+const test = (a: number, b: number): number => {
+  return a + b
+}
+
+console.log(test(1, 'ss'))
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
