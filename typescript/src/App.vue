f02dede5780e415d68c3f0805799f5959c68f194
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2021-10-20 13:14:52 +0800

    class 建制

--- typescript/src/App.vue
+++ typescript/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/13">13类</router-link> |
   </div>
   <router-view/>
 </template>
---
+++ typescript/src/views/13类.vue
@@ -0,0 +1,29 @@
+
+<script setup lang="ts">
+class Person {
+    // 定义 实例属性 需要使用new
+    name = 'Sam'
+
+    // 使用readolny 只能读取
+    readonly age=20
+
+    // 定义方法
+    sayHlleo = () => {
+      console.log('hello' + this.name)
+    }
+
+    // 使用static静态属性 不需要new 直接使用
+    static readonly sex = 1
+
+    static sayhello2 = () => {
+      console.log('hello2')
+    }
+}
+
+const per = new Person()
+console.log(per)
+console.log(Person.sex)
+per.sayHlleo()
+Person.sayhello2()
+
+</script>
